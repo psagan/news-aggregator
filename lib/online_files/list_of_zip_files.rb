24b716afb1addccc5d6a0d96cd124f6be7e1103f
psagan <patryk.sagan@icloud.com>
2016-05-14 11:25:16 +0200

    Add namespace to ListOfZipFiles

--- lib/online_files/list_of_zip_files.rb
+++ lib/online_files/list_of_zip_files.rb
@@ -1,29 +1,31 @@
-class ListOfZipFiles
-  PATTERN = /\d{13}\.zip/i
+module OnlineFiles
+  class ListOfZipFiles
+    PATTERN = /\d{13}\.zip/i
 
-  def initialize(host)
-    @host = host
-  end
-
-  def files
-    @files ||= extract_files
-  end
-
-  private
-
-  def extract_files
-    list = response.body.scan(PATTERN)
-    list.uniq
-  end
-
-  def response
-    unless @response
-      res = Net::HTTP.get_response(URI(host))
-      raise "Can't access the host" unless res.is_a?(Net::HTTPSuccess)
-      @response = res
+    def initialize(host)
+      @host = host
     end
-    @response
-  end
 
-  attr_reader :host
+    def files
+      @files ||= extract_files
+    end
+
+    private
+
+    def extract_files
+      list = response.body.scan(PATTERN)
+      list.uniq
+    end
+
+    def response
+      unless @response
+        res = Net::HTTP.get_response(URI(host))
+        raise "Can't access the host" unless res.is_a?(Net::HTTPSuccess)
+        @response = res
+      end
+      @response
+    end
+
+    attr_reader :host
+  end
 end
