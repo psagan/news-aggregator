086352a82b19a127e23b2417df8e9578454a9312
psagan <patryk.sagan@icloud.com>
2016-05-14 09:01:36 +0200

    Add required gems to autoloader

--- simple_autoloader.rb
+++ simple_autoloader.rb
@@ -1,5 +1,11 @@
 # this is simple autoloader for this small application
 
+# require gems
+require 'rubygems'
+require 'zip'
+require 'net/http'
+require 'redis'
+
 # add current path to $LOAD_PATH
 $: << File.join(File.dirname(__FILE__))
 
